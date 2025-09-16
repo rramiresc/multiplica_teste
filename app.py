50c3a77a127d46ff291246576c300cb9bb357ee6
rramiresc <rafaelramires@prof.educacao.sp.gov.br>
2025-09-16 17:53:03 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -362,7 +362,7 @@ def login_required(access_level_required):
             
             return fn(*args, **kwargs)
         return decorated_view
-    return wrapper
+    return decorated_view
     
 def allowed_file(filename):
     return '.' in filename and \
